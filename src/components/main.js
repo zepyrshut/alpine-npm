9b2c8defee34de50d9493fce76110a8949763cf2
Pedro Pérez <pedro@zepyrshut.com>
2023-01-28 01:47:17 +0100

    Envío de datos de formulario
    En el que se incluye al usuario qué opcione está seleccionada con la
    ayuda de Alpinejs. Se aprovecha el uso del ciclo de vida.

--- src/components/main.js
+++ src/components/main.js
@@ -2,11 +2,17 @@ import Alpine from 'alpinejs'
 import { loveCounter } from './loveCounter'
 import { fetchSomeData } from './fetchSomeData'
 import { contactForm } from './contactForm'
+import { submitSomeData } from './submitSomeData'
 
 window.Alpine = Alpine
 
 Alpine.data('loveCounter', loveCounter)
 Alpine.data('fetchSomeData', fetchSomeData)
 Alpine.data('contactForm', contactForm)
+Alpine.data('label', () => ({ text: 'Hello World'}))
+Alpine.data('submitSomeData', submitSomeData)
 
 Alpine.start()
+
+// son funciones que retornan un objeto con los datos
+// que se necesite en data
---
+++ src/components/submitSomeData.js
@@ -0,0 +1,37 @@
+export function submitSomeData() {
+  return {
+    people: [],
+    selected: "",
+    isLoading: false,
+    init() {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((json) => {
+          this.people = json;
+        });
+    },
+    submit() {
+      this.isLoading = true;
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          name: this.selected.name,
+          email: this.selected.email,
+          userId: this.selected.id,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          this.isLoading = false;
+          alert("Data saved!");
+        });
+    },
+    selection(selected) {
+      this.selected = selected;
+    },
+  };
+}
